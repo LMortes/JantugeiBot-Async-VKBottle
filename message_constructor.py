601c45cd558732fa2ff1ecc9279d88f408169dfa
aleks kutuzov ♡ <124031221+alekskutuzov@users.noreply.github.com>
2023-02-22 20:10:46 +0300

    Update message_constructor.py

--- message_constructor.py
+++ message_constructor.py
@@ -45,21 +45,21 @@ async def construct_message_info_admin(admin_info):
 
 
 async def construct_message_info_blacklist(player_info_black):
-    message_info_black = 'Информация о игроке находящемся в черном списке:\n\n'\
-                         f'Игровой ник: {player_info_black["player_info"][2]}\n' \
-                         f'ЧС структуры: {player_info_black["player_info"][1]}\n' \
-                         f'Степень ЧСа: {player_info_black["player_info"][3]}\n' \
-                         f'Причина занесения в ЧС: {player_info_black["player_info"][4]}\n' \
-                         f'Дата занесения в ЧС: {player_info_black["player_info"][5]}\n' \
-                         f'Прошло дней с момента занесения в ЧС: {(datetime.date.today() - player_info_black["player_info"][5]).days}\n' \
-                         f'Ник администратора, который занес игрока в ЧС: {player_info_black["player_info"][6]}\n' \
-                         'Количество смен: В разработке\n' \
-                         'Дата последней смены: В разработке'
+    message_info_black = '📝  Информация о игроке находящемся в черном списке:\n\n'\
+                         f'👤 Игровой ник: {player_info_black["player_info"][2]}\n' \
+                         f'📋 ЧС структуры: {player_info_black["player_info"][1]}\n' \
+                         f'🔢 Степень ЧСа: {player_info_black["player_info"][3]}\n' \
+                         f'📁 Причина занесения в ЧС: {player_info_black["player_info"][4]}\n' \
+                         f'📅 Дата занесения в ЧС: {player_info_black["player_info"][5]}\n' \
+                         f'📅 Прошло дней с момента занесения в ЧС: {(datetime.date.today() - player_info_black["player_info"][5]).days}\n' \
+                         f'👤 Ник администратора, который занес игрока в ЧС: {player_info_black["player_info"][6]}\n' \
+                         '🕗 Количество смен: В разработке\n' \
+                         '📅 Дата последней смены: В разработке'
     return message_info_black
 
 
 async def construct_message_info_formaccess(forms_info):
-    message_info_formaccess = 'Пользователи с доступом к форме:\n'
+    message_info_formaccess = '📂 Пользователи с доступом к форме:\n'
     counter = 1
     for row in forms_info["info_formaccess"]:
         user_info = await bot.api.users.get(row[1])
@@ -67,9 +67,9 @@ async def construct_message_info_formaccess(forms_info):
         user_last_name = user_info[0].last_name
         condition = ''
         if row[5] == 0:
-            condition = 'На заполнении'
+            condition = '📤 На заполнении'
         elif row[5] == 1:
-            condition = 'На одобрении'
+            condition = '📥 На одобрении'
         message_info_formaccess += f'{counter}. [id{row[1]}|{user_first_name} {user_last_name}] | Тип формы: {row[2]} | Адм. выдавший форму: [id{row[3]}|{row[4]}] | Состояние: {condition}\n'
         counter += 1
     return message_info_formaccess
